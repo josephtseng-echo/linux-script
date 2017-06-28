73c3ee455df3deb0d339a913b0e81c054f88b0ab
josephzeng <josephzeng36@gmail.com>
2017-06-28 22:16:20 +0800

    新增一种写法

--- script/drop_caches.sh
+++ script/drop_caches.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 #当free -m  cached超过6000 进行释放内存
+#第一种写法
 str=$( free -m )
 arr=(${str// / })
 check=$(expr ${arr[12]} + 0)
 if [ ${check} -gt 6000  ];then
-    ( echo 1 >  /proc/sys/vm/drop_cache )
+    ( echo 3 >  /proc/sys/vm/drop_caches )
+fi
+
+#第二种写法
+freeMem=$(free -m|awk '/buffers\/cache/{print $4}')
+MaxValue=6000
+
+if [ ${freeMem} -gt ${MaxValue}  ];then
+    ( echo 3 >  /proc/sys/vm/drop_caches )
 fi
